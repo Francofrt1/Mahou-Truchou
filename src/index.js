26d2ffdbd9215859cdd8497f061160c39805b253
Francofrt1 <80823509+Francofrt1@users.noreply.github.com>
2024-09-12 21:34:04 -0300

    Add day/night cycle wip, and correct bg size

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ export class Game {
     constructor() {
         this.app = new PIXI.Application();
         this.character;
+        this.bgContainer = new PIXI.Container();
+        this.backgroundsCicle = {}
         this.currentBackground;
         this.inputKeys = {};
 
@@ -29,11 +31,8 @@ export class Game {
 
     async loadGameElements() {
         await PIXI.Assets.init({manifest: "./assets/manifest.json"});
-        const playerCharacterAssets = await PIXI.Assets.loadBundle('player-bundle');
-        const dayBackgroundAssets = await PIXI.Assets.loadBundle('day-background-bundle');
-
-        this.currentBackground = new Background(this, dayBackgroundAssets);
-        this.character = new Character(this, playerCharacterAssets.character);
+        this.loadPlayerCharacter();
+        await this.loadBackgroundsCicle();
     }
 
     async gameLoop() {
@@ -48,6 +47,49 @@ export class Game {
         window.onkeyup = (e) => {
             delete this.inputKeys[e.key.toLowerCase()];
         }
+    }
+
+    async loadBackgroundsCicle() {
+        const dayBackgroundAssets = await PIXI.Assets.loadBundle('day-background-bundle');
+        let dayBg = new Background(this, dayBackgroundAssets);
+        this.backgroundsCicle["day"] = dayBg;
+        await this.setCurrentBackground(dayBg);
+        this.app.stage.addChild(this.bgContainer);
+        
+        this.backgroundsCicle["day-2"] = new Background(this, await PIXI.Assets.loadBundle('day-background-2-bundle'));
+        this.backgroundsCicle["evening"] = new Background(this, await PIXI.Assets.loadBundle('evening-background-bundle'));
+        this.backgroundsCicle["evening-2"] = new Background(this, await PIXI.Assets.loadBundle('evening-background-2-bundle'));
+        this.backgroundsCicle["night"] = new Background(this, await PIXI.Assets.loadBundle('night-background-bundle'));
+        this.backgroundsCicle["night-2"] = new Background(this, await PIXI.Assets.loadBundle('night-background-2-bundle'));
+        this.backgroundsCicle["morning"] = new Background(this, await PIXI.Assets.loadBundle('morning-background-bundle'));
+        this.backgroundsCicle["morning-2"] = new Background(this, await PIXI.Assets.loadBundle('morning-background-2-bundle'));
+
+        this.cicleThroughBgs();
+    }
+
+    async cicleThroughBgs() {
+        setInterval(
+            (e) => {
+                const keys = Object.keys(e.backgroundsCicle).map((k) => e.backgroundsCicle[k]);
+                let nextIndx = keys.indexOf(e.currentBackground) + 1;
+                nextIndx = nextIndx >= keys.length ? 0 : nextIndx;
+                const nextBg = keys.at(nextIndx);
+                e.setCurrentBackground(nextBg);
+            }
+        , 40000
+        , this
+        );
+    }
+
+    async loadPlayerCharacter() {
+        const playerCharacterAssets = await PIXI.Assets.loadBundle('player-bundle');
+        this.character = new Character(this, playerCharacterAssets.character);
+    }
+
+    async setCurrentBackground(newBg) {
+        this.currentBackground = newBg;
+        this.bgContainer.removeChildren();
+        this.bgContainer.addChild(this.currentBackground.container);
     }
 }
 
--- src/scripts/background.js
+++ src/scripts/background.js
@@ -4,7 +4,7 @@ export class Background {
         this.container = new PIXI.Container();
         this.bgParts = [];
         this.bgX = 0;
-        this.bgSpeed = 0.5;
+        this.bgSpeed = 0.1;
         this.load(backgroundAsset)
     }
 
@@ -15,11 +15,15 @@ export class Background {
             let texture = backgroundAsset['background-' + (i + 1)];
             let backgroundTileSprite = new PIXI.TilingSprite({texture, width:this.game.app.screen.width, height:this.game.app.screen.height});
             backgroundTileSprite.position.set(0,0);
+            
+            
+            let scale = Math.max( window.innerWidth / backgroundTileSprite.texture.width, window.innerHeight / backgroundTileSprite.texture.height);
+            backgroundTileSprite.scale.set(scale,scale);
+            backgroundTileSprite.x = Math.round( ( window.innerWidth - backgroundTileSprite.width ) / 2);
+            backgroundTileSprite.y = Math.round( ( window.innerHeight - backgroundTileSprite.height ) / 2);
             this.bgParts.push(backgroundTileSprite);
             this.container.addChild(backgroundTileSprite);
         }
-
-        this.game.app.stage.addChild(this.container);
     }
 
     async update() {
